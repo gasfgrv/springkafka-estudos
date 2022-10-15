40582dd9c6b699dc3f6adc0850108d2a14667537
Gustavo Silva <gusta@DESKTOP-DUAV7CL.localdomain>
2022-10-15 20:42:04 -0300

    headers e metadada do consumer

--- consumer/src/main/java/br/com/gusta/springkafka/consumer/listener/TesteListener.java
+++ consumer/src/main/java/br/com/gusta/springkafka/consumer/listener/TesteListener.java
@@ -3,6 +3,8 @@ package br.com.gusta.springkafka.consumer.listener;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.listener.adapter.ConsumerRecordMetadata;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -10,9 +12,12 @@ import org.springframework.stereotype.Component;
 public class TesteListener {
 
     @KafkaListener(topics = { "topic-1" }, groupId = "group-1")
-    public void listen(String message, ConsumerRecordMetadata metadata) {
+    public void listen(String message,
+                       ConsumerRecordMetadata metadata,
+                       @Header(KafkaHeaders.RECEIVED_TOPIC) String topic
+    ) {
         log.info("Topic: {} - Partition: {} - Message: {}",
-                metadata.topic(),
+                topic,
                 metadata.partition(),
                 message);
     }
