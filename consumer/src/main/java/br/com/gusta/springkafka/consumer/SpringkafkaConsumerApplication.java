47182f15120619c816cef36e40211a50df053519
Gustavo Silva <gusta@DESKTOP-DUAV7CL.localdomain>
2022-10-15 17:26:54 -0300

    arquivos iniciais

---
+++ consumer/src/main/java/br/com/gusta/springkafka/consumer/SpringkafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package br.com.gusta.springkafka.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringkafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringkafkaConsumerApplication.class, args);
+	}
+
+}
---
+++ producer/src/main/java/br/com/gusta/springkafka/producer/SpringkafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package br.com.gusta.springkafka.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringkafkaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringkafkaProducerApplication.class, args);
+	}
+
+}
